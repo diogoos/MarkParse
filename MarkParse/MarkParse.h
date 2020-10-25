65dbde6c1e6c634d39052da75648661ee6741c4d
daemonleaf <59505963+daemonleaf@users.noreply.github.com>
2020-10-25 11:45:55 -0300

    Initial Commit

---
+++ MarkParse/MarkParse.h
@@ -0,0 +1,18 @@
+//
+//  MarkParse.h
+//  MarkParse
+//
+//  Created by Diogo Silva on 10/25/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarkParse.
+FOUNDATION_EXPORT double MarkParseVersionNumber;
+
+//! Project version string for MarkParse.
+FOUNDATION_EXPORT const unsigned char MarkParseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkParse/PublicHeader.h>
+
+
